b21ae682bd08400a3f3a65eeeaba7d6d2192b2ae
thelastkiss113 <76501819+thelastkiss113@users.noreply.github.com>
2024-10-18 13:34:01 -0700

    second commit

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ app.listen(PORT, () => {
 })
 
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home"); // Render home.ejs
+});
+
+app.get("/about", (req, res) => {
+    res.render("about"); // Render about.ejs
+});
+
+
+app.post("submit")
